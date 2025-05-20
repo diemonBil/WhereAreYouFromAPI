d733f09db4df1648864c12ee5a6f13dc05d85502
diemonBil <crazyevildocs@gmail.com>
2025-05-20 12:50:07 +0300

    Update settings.py with installed apps

--- WhereAreYouFromAPI/settings.py
+++ WhereAreYouFromAPI/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'name_origin'
 ]
 
 MIDDLEWARE = [
